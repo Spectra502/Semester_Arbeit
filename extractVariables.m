b83c1a0aa91d9b686ba64993329e081eb842ced6
Spectra <spectra502@gmail.com>
2024-07-08 14:36:31 +0200

    updated variable extraction and feature selection

--- extractVariables.m
+++ extractVariables.m
@@ -1,6 +1,44 @@
-function [date, hour, torque, velocity] = extractVariables(inputString)
-    date = inputString(1:8);
-    hour = inputString(10:17);
-    torque = extractBetween(inputString, '112_5_', '_Nm');
-    velocity = extractBetween(inputString, '_Nm_', '_rpm');
+function [label, torque, speed, damageLabel, damageType] = extractVariables(inputString)
+    
+    % Determine the label based on the presence of "GF"
+    if contains(inputString, 'GF')
+        label = 'damaged';
+        damageLabel = 1;
+        damageType = 'micropitting';
+    else
+        label = 'healthy';
+        damageLabel = 0;
+        damageType = 'none';
+    end
+    
+   pattern = '-112_5_';
+   length(pattern);
+   startIndex = strfind(inputString, pattern);
+
+   if ~isempty(startIndex)
+       startIndex = startIndex + length(pattern);
+       remainderString = inputString(startIndex:end);
+    
+       numPattern = '(\d+)';
+       tokens = regexp(remainderString, numPattern, 'tokens');
+
+       % Debug: Print the remainder string and tokens
+        disp('Remainder String:');
+        disp(remainderString);
+        %disp('Tokens:');
+        %disp(tokens);
+        
+        if length(tokens) >= 2
+            torque = str2double(tokens{1})
+            speed = str2double(tokens{2})
+        else
+            torque = NaN;
+            speed = NaN;
+        end
+    else
+        torque = NaN;
+        speed = NaN;
+        tokens = {};
+    end
+       
 end
